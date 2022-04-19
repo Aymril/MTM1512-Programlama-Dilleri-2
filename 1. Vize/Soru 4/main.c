d7ef8b2b7836b703611bda63f9ecfa906c6043ae
Imral Okyar <100049698+Aymril@users.noreply.github.com>
2022-04-19 18:49:52 +0300

    Create main.c

---
+++ 1. Vize/Soru 4/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int* max_address(int* A, int n) {
+    int *result = A, max = A[0];
+    int i;
+    for (i = 1; i < n; ++i) {
+        if (A[i] > max) {
+            max = A[i];
+            result = &A[i];
+        }
+    }
+    return result;
+}
+
+void printArray(int* A, int n) {
+    int i;
+    for (i = 0; i < n; ++i) {
+        printf("%3d", A[i]);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    int A[5] = {1, 1, 2, 3, 5};
+    int B[6] = {8, 13, 21, 34, 55, 89};
+    int i, temp, *p, *q;
+
+    p = max_address(A, 5);
+    q = max_address(B, 3);
+
+    temp = *p;
+    *p = *q;
+    *q = temp;
+
+    printArray(A, 5);
+    printArray(B, 6);
+}
